7e44bbcce68851e10607d45b323ca1048d9b2197
Kieren Davies <kieren@kdavi.es>
2015-04-29 18:32:22 +0200

    Prevent blank lines in Feed.toString

--- src/main/scala/za/co/kierendavies/tweetfeed/Feed.scala
+++ src/main/scala/za/co/kierendavies/tweetfeed/Feed.scala
@@ -1,5 +1,8 @@
 package za.co.kierendavies.tweetfeed
 
 class Feed(val user: User, val tweets: Seq[Tweet]) {
-  override def toString = s"$user\n\t${tweets.mkString("\n\t")}\n"
+  override def toString = {
+    if (tweets.isEmpty) user.toString
+    else s"$user\n\t${tweets.mkString("\n\t")}"
+  }
 }
