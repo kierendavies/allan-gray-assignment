72f2f897b5445ec7f5d7f9c8fa2595b38804a93f
Kieren Davies <kieren@kdavi.es>
2015-04-29 18:12:44 +0200

    Fully parse files

--- src/main/scala/za/co/kierendavies/tweetfeed/TweetFileParser.scala
+++ src/main/scala/za/co/kierendavies/tweetfeed/TweetFileParser.scala
@@ -1,7 +1,19 @@
 package za.co.kierendavies.tweetfeed
 
 object TweetFileParser extends FileParser {
-  def parseLine(line: String): Unit = {
-    
+  val userNameRegex = "[a-zA-Z0-9_]+".r
+  val tweetRegex = s"($userNameRegex)> (.{1,140})".r
+  val tooLongTweetRegex = s"$userNameRegex> .{141,}".r
+
+  def parseLine(line: String): Unit = line match {
+    case tweetRegex(userName, content) =>
+      User.byName(userName).tweet(content)
+
+    case tooLongTweetRegex() =>
+      throw LineParsingException(s"tweet too long: $line")
+
+    case "" => throw LineParsingException("empty line")
+
+    case _ => throw LineParsingException(s"could not parse: $line")
   }
 }
--- src/main/scala/za/co/kierendavies/tweetfeed/UserFileParser.scala
+++ src/main/scala/za/co/kierendavies/tweetfeed/UserFileParser.scala
@@ -1,7 +1,19 @@
 package za.co.kierendavies.tweetfeed
 
+import scala.util.matching.Regex
+
 object UserFileParser extends FileParser {
-  def parseLine(line: String): Unit = {
-    
+  val userNameRegex = "[a-zA-Z0-9_]+".r
+  val followsRegex = s"($userNameRegex) follows ($userNameRegex(?:, $userNameRegex)*)".r
+
+  def parseLine(line: String): Unit = line match {
+    case followsRegex(userName, following) =>
+      following.split(", ").foreach {
+        User.byName(userName).follow(_)
+      }
+
+    case "" => throw LineParsingException("empty line")
+
+    case _ => throw LineParsingException(s"could not parse: $line")
   }
 }
