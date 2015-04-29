2dec61794b47894974be3c17a94ef4100f9ca120
Kieren Davies <kieren@kdavi.es>
2015-04-29 09:47:52 +0200

    Handle args more neatly

--- src/main/scala/za/co/kierendavies/tweetfeed/Application.scala
+++ src/main/scala/za/co/kierendavies/tweetfeed/Application.scala
@@ -6,15 +6,16 @@ import scopt.OptionParser
 
 object Application extends App {
   val parser = new OptionParser[Config]("tweetfeed") {
-    opt[File]('u', "users") required() action { (f, c) => c.copy(userFile = f) }
-    opt[File]('t', "tweets") required() action { (f, c) => c.copy(userFile = f) }
+    opt[File]('u', "users") required() valueName("<file>") action {
+      (f, c) => c.copy(userFile = f) }
+    opt[File]('t', "tweets") required() valueName("<file>") action {
+      (f, c) => c.copy(userFile = f) }
   }
   parser.parse(args, Config()) match {
+    case None => System.exit(1)
+
     case Some(config) =>
       println(config.userFile.getPath)
       println(config.tweetFile.getPath)
-
-    case None =>
-      println("bad arguments")
   }
 }
